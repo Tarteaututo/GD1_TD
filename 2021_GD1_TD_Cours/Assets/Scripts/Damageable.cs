2bb41ad553ca4d6106d3b2dab6bf1ffdc84d9e13
alex <alexandre@gameleon-studio.fr>
2021-01-06 16:34:16 +0100

    ADD Damageable and begin Projectile

---
+++ 2021_GD1_TD_Cours/Assets/Scripts/Damageable.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+    [SerializeField]
+    private int _health = 2;
+
+    private int _currentHealth = 0;
+
+    private void Awake()
+    {
+        _currentHealth = _health;
+    }
+
+    public void DoDamage(int damage)
+    {
+        _currentHealth -= damage;
+    }
+}
---
+++ 2021_GD1_TD_Cours/Assets/Scripts/Projectile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        // vérifier si le collider est un damageable
+        Damageable damageable = other.GetComponentInParent<Damageable>();
+        if (damageable != null)
+        {
+            damageable.DoDamage(1);
+        }
+
+        // si oui
+        // damageable.DoDamage()
+        // Destroy itself
+    }
+}
